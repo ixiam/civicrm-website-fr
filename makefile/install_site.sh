7fb1655e85947df60342acfe9e1066f052293aa7
Luciano Spiegel <l.spiegel@ixiam.com>
2014-01-30 19:01:07 +0100

    install site bash script

--- makefile/install_site.sh
+++ makefile/install_site.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 DRUPAL_WWW="/var/www/civicrm_website_i18n"
+# MAKEFILE="<git repo path>/makefile/general.make"
 MAKEFILE="/home/ixiam/civicrm_website_i18n/makefile/general.make"
 DB_NAME="civicrm_website_i18n"
 DB_USER="root"
 DB_PASS="admin"
+DRUPAL_USER="root"
+DRUPAL_PASS="admin"
+
 
 clear
 # Clean previous installations
@@ -14,8 +18,8 @@ drush make $MAKEFILE $DRUPAL_WWW
 # Drush site-install
 cd $DRUPAL_WWW
 chmod -R 777 $DRUPAL_WWW/sites/default
-drush --yes si civicrm_website_i18n --db-url=mysql://$DB_USER:$DB_PASS@localhost:/$DB_NAME --db-su=$DB_USER --db-su-pw=$DB_PASS --site-name="CiviCRM in Spanish / French" --account-mail="l.spiegel@ixiam.com" --account-name=root --account-pass=admin
+drush --yes si civicrm_website_i18n --db-url=mysql://$DB_USER:$DB_PASS@localhost:/$DB_NAME --db-su=$DB_USER --db-su-pw=$DB_PASS --site-name="" --account-name=$DRUPAL_USER --account-pass=$DRUPAL_PASS
 
-# Change permissions
+# Change permissions, just in case...
 chmod -R 777 $DRUPAL_WWW/sites/default/files
 chmod -R 777 $DRUPAL_WWW/sites/default
